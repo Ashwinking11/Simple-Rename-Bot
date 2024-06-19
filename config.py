7f0540b8dd2c19c31163ef2fc5e06969e360f0e5
Ashwinking11 <161299765+Ashwinking11@users.noreply.github.com>
2024-06-19 16:53:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
-CAPTION = environ.get("CAPTION", "")
+API_ID = int(environ.get("API_ID", "28015531"))
+API_HASH = environ.get("API_HASH", "2ab4ba37fd5d9ebf1353328fc915ad28")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7072790150:AAGl-Vjbu0mZJzGHii36Ax-cRR1j5wxZW7M")
+ADMIN = int(environ.get("ADMIN", "6121610691"))          
+CAPTION = environ.get("CAPTION", "@animetamil_xyz")
 
 # for thumbnail ( back end is MrMKN brain 😉)
 DOWNLOAD_LOCATION = "./DOWNLOADS"
