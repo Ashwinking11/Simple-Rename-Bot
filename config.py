dbd8b518087a89413c546b1b04ee99bad252898b
Ashwinking11 <161299765+Ashwinking11@users.noreply.github.com>
2024-06-19 16:56:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ API_ID = int(environ.get("API_ID", "28015531"))
 API_HASH = environ.get("API_HASH", "2ab4ba37fd5d9ebf1353328fc915ad28")
 BOT_TOKEN = environ.get("BOT_TOKEN", "7072790150:AAGl-Vjbu0mZJzGHii36Ax-cRR1j5wxZW7M")
 ADMIN = int(environ.get("ADMIN", "6121610691"))          
-CAPTION = environ.get("CAPTION", "@animetamil_xyz")
+CAPTION = environ.get("CAPTION", "fals")
 
 # for thumbnail ( back end is MrMKN brain 😉)
 DOWNLOAD_LOCATION = "./DOWNLOADS"
